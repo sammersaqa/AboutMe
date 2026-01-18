3d40403a386dcfdd16fec72a87609b19ffb163ca
sammersaqa <SammerSaqa@gmail.com>
2026-01-17 18:53:11 -0800

    initial commit

---
+++ css/scripts/main.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.getElementById("backToTop");
+
+    window.addEventListener("scroll", () => {
+        btn.style.display = window.scrollY > 300 ? "block" : "none";
+    });
+
+    btn.addEventListener("click", () => {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    });
+});
